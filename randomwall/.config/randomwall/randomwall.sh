eab7c34a5c2a5dd4c070ed33fd346f92ebbcc2ea
Ralph Bolliger <ralph.bolliger@me.com>
2025-03-08 14:18:56 +0100

    Got moar catppuccin-style wallpapers, changed hyprpaper config to randomly change wallpaper using systemd service and timer

---
+++ randomwall/.config/randomwall/randomwall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+WALLPAPER_DIR="$HOME/.config/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+hyprctl hyprpaper reload ,"$WALLPAPER"
