2452539c0d83629615ecaafed6bf96fd11860d22
Ralph Bolliger <ralph.bolliger@me.com>
2025-03-17 00:46:24 +0100

    Pretty happy. For now.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,25 +1,65 @@
 return {
   {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    'nvim-telescope/telescope.nvim',
+    opts = {
+      defaults = {
+        border = true,
+        promt_prefix = '🔍 ',
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          '--no-ignore',
+        },
+      },
+    },
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
+    'stevearc/conform.nvim',
+    event = 'BufWritePre',
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        javascript = { 'biome' },
+        javascriptreact = { 'biome' },
+        typescript = { 'biome' },
+        typescriptreact = { 'biome' },
+        json = { 'biome' },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+      },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function ()
+      return require ('configs.lspconfig')
     end,
   },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = {
+        'vim',
+        'lua',
+        'vimdoc',
+        'html',
+        'css',
+        'typescript',
+        'tsx',
+      },
+    },
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {},
+  },
 }
