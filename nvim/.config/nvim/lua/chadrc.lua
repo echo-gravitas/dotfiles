005a3b2b39d41964db2ac0ac962fe68649d628a2
Ralph Bolliger <ralph.bolliger@me.com>
2025-03-17 16:54:35 +0100

    Done for now bringing NvChad to Nvim

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,17 +1,7 @@
--- This file needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :(
-
----@type ChadrcConfig
 local M = {}
 
 M.base46 = {
   theme = 'catppuccin',
-
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
 }
 
 M.nvdash = {
@@ -38,9 +28,12 @@ M.ui = {
   telescope = {
     style = 'bordered',
   },
+
   statusline = {
     theme = 'default',
+    separator_style = 'round',
   },
+
   tabufline = {
     lazyload = false,
   },
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   {
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    opts = {
+      delay = 0,
+    },
+  },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    opts = {
+      signs = true,
+    },
+  },
+  {
     'nvim-telescope/telescope.nvim',
     opts = {
       defaults = {
@@ -67,20 +81,144 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     opts = {
+      auto_install = true,
+      highlight = {
+        enabled = true,
+      },
+      indent = {
+        enabled = true,
+      },
       ensure_installed = {
-        'vim',
-        'lua',
-        'vimdoc',
-        'html',
+        'bash',
         'css',
-        'typescript',
+        'csv',
+        'git_config',
+        'gitcommit',
+        'gitignore',
+        'gpg',
+        'html',
+        'http',
+        'hyprlang',
+        'javascript',
+        'json',
+        'jsonc',
+        'liquid',
+        'lua',
+        'nginx',
+        'php',
+        'powershell',
+        'python',
+        'regex',
+        'scss',
+        'sql',
+        'ssh_config',
+        'svelte',
+        'toml',
+        'tsv',
         'tsx',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'vue',
+        'yaml',
       },
     },
   },
   {
-    'nvim-tree/nvim-tree.lua',
-    opts = {},
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '┃' },
+        delete = { text = '-' },
+        topdelete = { text = '-' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '+' },
+        change = { text = '┃' },
+        delete = { text = '-' },
+        topdelete = { text = '-' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged_enable = true,
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      watch_gitdir = {
+        follow_files = true,
+      },
+      auto_attach = true,
+      attach_to_untracked = false,
+      current_line_blame = false,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil,
+      max_file_length = 40000,
+      preview_config = {
+
+        border = 'single',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+      },
+    },
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    init = function ()
+      vim.api.nvim_set_hl (0, 'CatppuccinMauve', { fg = '#cba6f7' })
+      vim.api.nvim_set_hl (0, 'CatppuccinRed', { fg = '#f38ba8' })
+      vim.api.nvim_set_hl (0, 'CatppuccinGreen', { fg = '#a6e3a1' })
+      vim.api.nvim_set_hl (0, 'CatppuccinLavender', { fg = '#b4befe' })
+      vim.api.nvim_set_hl (0, 'CatppuccinFlamingo', { fg = '#f2cdcd' })
+      vim.api.nvim_set_hl (0, 'CatppuccinSapphire', { fg = '#74c7ec' })
+      vim.api.nvim_set_hl (0, 'CatppuccinPeach', { fg = '#fab387' })
+    end,
+    opts = {
+      scope = {
+        enabled = true,
+      },
+      indent = {
+        char = '┃',
+        -- highlight = {
+        --   'CatppuccinMauve',
+        --   'CatppuccinRed',
+        --   'CatppuccinGreen',
+        --   'CatppuccinLavender',
+        --   'CatppuccinFlamingo',
+        --   'CatppuccinSapphire',
+        --   'CatppuccinPeach',
+        -- },
+      },
+    },
+  },
+  {
+    'echasnovski/mini.nvim',
+    version = '*',
+    lazy = false,
+    config = function ()
+      require ('mini.cursorword').setup ()
+      require ('mini.ai').setup ()
+      -- require ('mini.animate').setup ()
+      require ('mini.hipatterns').setup ()
+    end,
   },
 }
