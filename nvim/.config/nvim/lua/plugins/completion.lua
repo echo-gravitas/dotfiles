15e93dc57be50f0049b0c1f9a15f7045eb34b501
Ralph Bolliger <ralph.bolliger@me.com>
2025-04-10 23:24:35 +0200

    Some (unnecessary) changes

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   'saghen/blink.cmp',
   dependencies = {
     'rafamadriz/friendly-snippets',
-  },
+    'Kaiser-Yang/blink-cmp-avante', },
 
   version = '1.*',
   opts = {
@@ -29,11 +29,11 @@ return {
           columns = { { 'kind_icon' }, { 'label', gap = 1 } },
           components = {
             label = {
-              text = function (ctx)
-                return require ('colorful-menu').blink_components_text (ctx)
+              text = function(ctx)
+                return require('colorful-menu').blink_components_text(ctx)
               end,
-              highlight = function (ctx)
-                return require ('colorful-menu').blink_components_highlight (ctx)
+              highlight = function(ctx)
+                return require('colorful-menu').blink_components_highlight(ctx)
               end,
             },
           },
@@ -43,10 +43,18 @@ return {
 
     sources = {
       default = {
+        'avante',
         'lsp',
         'path',
         'snippets',
         'buffer',
+      },
+      providers = {
+        avante = {
+          module = 'blink-cmp-avante',
+          name = 'Avante',
+          opts = {},
+        },
       },
     },
     fuzzy = {
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,15 +1,38 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
   opts = {
-    theme = 'nord',
-    padding = 5,
+    options = {
+      theme = 'ayu_mirage',
+      padding = 1,
+      disabled_filetypes = {
+        'Avante',
+        'AvanteSelectedFiles',
+        'AvanteInput',
+      },
+    },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename' },
-      lualine_x = { 'lsp_status', 'fileformat', 'filetype' },
-      lualine_z = { 'location' },
+      lualine_a = {
+        'mode',
+      },
+      lualine_b = {
+        'branch',
+        'diff',
+        'diagnostics',
+      },
+      lualine_c = {
+        'filename',
+      },
+      lualine_x = {
+        'lsp_status',
+        'fileformat',
+        'filetype',
+      },
+      lualine_z = {
+        'location',
+      },
     },
   },
 }
