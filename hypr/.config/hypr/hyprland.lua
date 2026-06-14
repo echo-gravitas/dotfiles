908d6aea125551ddd167d4ff61b1f304cad2dbb4
Ralph Bolliger <ralph.bolliger@pm.me>
2026-06-14 23:06:12 +0200

    removed set random wall timer and service, configured hyprpaper

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -18,9 +18,9 @@ hl.monitor({
 })
 
 hl.on("hyprland.start", function()
+  hl.exec_cmd("hyprpaper")
   hl.exec_cmd("hypridle")
   hl.exec_cmd("waybar")
-  hl.exec_cmd("hyprpaper")
   hl.exec_cmd("vicinae server")
 end)
 
